d75ed4892f7708af81b4905be7542cef0384f617
Bankole614 <bankoleayobami01@gmail.com>
2025-07-18 11:55:29 +0100

    wip

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
+
 const port = process.env.PORT || 3000;
-
-app.get('/api/hello', (_req, res) => {
-  res.json({ message: '👋 Hello from your new backend!' });
-});
-
 app.listen(port, () => {
-  console.log(`🚀 Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
